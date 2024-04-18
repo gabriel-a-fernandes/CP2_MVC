48951809c70029cbc9d263dc92a755553a3b7f7a
Gabriel Fernandes <gabriel-a-fernandes@hotmail.com>
2024-04-18 19:45:03 -0300

    up

--- CP2_MVC/obj/Debug/net8.0/CP2_MVC.AssemblyInfo.cs
+++ CP2_MVC/obj/Debug/net8.0/CP2_MVC.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CP2_MVC")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c56ccf9a9918f34492d2824d3ada7ff124778658")]
 [assembly: System.Reflection.AssemblyProductAttribute("CP2_MVC")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CP2_MVC")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
